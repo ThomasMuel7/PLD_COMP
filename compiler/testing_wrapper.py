fe289f26d3a20ad76f181b5bd9029945c6c55023
Charlestonne <charles@cesbron.es>
2026-03-05 11:41:49 +0100

    fixMacOS

--- compiler/testing_wrapper.py
+++ compiler/testing_wrapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import subprocess
+import platform
 
 # Définition des codes couleurs ANSI
 GREEN = "\033[92m"
@@ -13,16 +14,24 @@ TEST_FILES_DIR = '../testfiles'
 
 def run_tests():
     print(f"Lancement des tests de {TEST_FILES_DIR}")
-    
+
+    # Détection de l'OS
+    cmd = ['python3', TEST_SCRIPT, TEST_FILES_DIR]
+
+    if platform.system() == "Darwin":  # macOS
+        cmd = ["arch", "-x86_64"] + cmd
+
     # Exécution du script de test
-    result = subprocess.run(['python3', TEST_SCRIPT, TEST_FILES_DIR], 
-                           stdout=subprocess.PIPE, 
-                           stderr=subprocess.PIPE, 
-                           text=True)
-    
+    result = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
     output = result.stdout
     lines = [line.strip() for line in output.splitlines() if line.strip()]
-    
+
     total_tests = 0
     passed_tests = 0
     failed_tests = []
@@ -32,9 +41,10 @@ def run_tests():
         if lines[i].startswith("TEST-CASE:"):
             total_tests += 1
             test_name = lines[i].replace("TEST-CASE:", "").strip()
-            
+
             if i + 1 < len(lines):
                 result_line = lines[i+1]
+                print(result_line)
                 if "TEST OK" in result_line:
                     passed_tests += 1
                 elif "TEST FAIL" in result_line:
@@ -44,11 +54,10 @@ def run_tests():
     print("\n" + "="*35)
     print(f"{BOLD}       RAPPORT DE TEST{RESET}")
     print("="*35)
-    
+
     if total_tests > 0:
         pass_percentage = (passed_tests / total_tests) * 100
-        
-        # Logique de couleur pour le taux de succès
+
         if pass_percentage > 80:
             rate_color = GREEN
         elif pass_percentage > 50:
@@ -60,13 +69,14 @@ def run_tests():
         print(f"Succès         : {GREEN}{passed_tests}{RESET}")
         print(f"Échecs         : {RED}{len(failed_tests)}{RESET}")
         print(f"Taux de succès : {rate_color}{pass_percentage:.2f}%{RESET}")
-        
+
         if failed_tests:
             print(f"\n{RED}{BOLD}Détail des tests échoués :{RESET}")
             for test in failed_tests:
                 print(f" {RED}- {test}{RESET}")
     else:
         print(f"{RED}Aucun test détecté. Vérifie tes chemins.{RESET}")
+
 
 if __name__ == "__main__":
     run_tests()
