17799cf286e473e2dd1074e0ea9b7710b889ff5d
Alex <alex@DESKTOP-G3SS467>
2026-03-05 12:17:59 +0100

    assign with tests work

---
+++ testfiles/op-assignment/add/add_assign_with_var.c
@@ -0,0 +1,9 @@
+int main()
+{
+  int a;
+  int b;
+  a = 4;
+  b = 5;
+  a += b;
+  return a; /* doit retourner 9 */
+}
---
+++ testfiles/op-assignment/div/div_assign.c
@@ -0,0 +1,7 @@
+int main()
+{
+  int a;
+  a = 20;
+  a /= 5;
+  return a; /* doit retourner 4 */
+}
